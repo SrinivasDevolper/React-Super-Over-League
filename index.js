86486486c16de6d564bb352fcc900f6b2ed0acc3
srinivasDevolper <kingsrinivassri123@gmail.com>
2024-04-26 06:26:17 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div class='bg-container'>
+    <div class='content'>
+      <h1 class='heading'> Super Over League</h1>
+      <div class='img-cont'>
+        <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' />
+        <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png' />
+      </div>
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
